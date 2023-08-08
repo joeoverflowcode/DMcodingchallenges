a9fe400fbddeb004633be8e6ed44ece840052608
Joe Aguado <aguado.joe@gmail.com>
2023-08-08 09:33:08 -0500

    First coding challenge -- Joe

---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 25;
+let y = 30;
+
+if (x > y){
+    console.log(x + " is larger than " + y)
+}
+if (y > x){
+    console.log(y + " is larger than " + x)
+}
+if (x == y){
+    console.log("The values are identical.")
+}
