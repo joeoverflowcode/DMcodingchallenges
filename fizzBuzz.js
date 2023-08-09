88cf841f081d3c0409fbe18314f941322b3e9f33
Joe Aguado <aguado.joe@gmail.com>
2023-08-09 09:53:03 -0500

    Cleaned up the readability -- Joe

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,9 @@
-for (let i = 1; i <= 50; i++ ){ // This instantiates i, sets it to 1 and goes to 50, and counts up 
-    if(i%3 ==0 && i %5==0)console.log('Fizzbuzz'); // This was moved from bottom row because program did not read FizzBuzz because of order of operations
-    else if(i%3 ==0) console.log('Fizz'); // %  operator with 3
-    else if(i%5 == 0) console.log('Buzz'); // % modulo operator with 5
-    console.log(i); //console log runs through i with else if conditions
+for (let i = 1; i <= 50; i++ ){     // This instantiates i, sets it to 1 and goes to 50, and counts up 
+    if(i%3 ==0 && i %5==0)
+        console.log('Fizzbuzz');    // This was moved from bottom row because program did not read FizzBuzz because of order of operations
+    else if(i%3 ==0) 
+        console.log('Fizz');       // %  operator with 3
+    else if(i%5 == 0) 
+        console.log('Buzz');     // % modulo operator with 5
+    console.log(i);             //console log runs through i with else if conditions
 }
